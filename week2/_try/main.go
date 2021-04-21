49e879cb7e1589afda8cc8454c9b37c05bb0ba97
ccccrrrr <zcr1006@gmail.com>
2021-04-21 16:50:32 +0800

    week2-3

--- week2/_try/main.go
+++ week2/_try/main.go
@@ -49,63 +49,67 @@ func main() {
 	// 3. serviceABC return an authorization page to APP
 	// a request was sent from APP to server
 	// and the server receive it and redirect to redirect page with code
-	server.GET("/server/authorization", func(c *gin.Context){
-		clientId := c.Query("client_id")
-		code := c.Query("response_type")
-		scope := "read-only"
-		redirect_uri := c.Query("redirect_uri")
-		if len(c.Query("scope")) == 0 {
-			scope = c.Query("scope")
-		}
-		db.Update(&AuthRequest{
-			ResponseType: code,
-			ClientId:     clientId,
-			RedirectUri:  redirect_uri,
-			Scope:        scope,
-		})
-		c.Redirect(http.StatusPermanentRedirect, "/server/" + redirect_uri + "?code=" + code)
-	})
+	//server.GET("/server/authorization", func(c *gin.Context){
+	//	clientId := c.Query("client_id")
+	//	code := c.Query("response_type")
+	//	scope := "read-only"
+	//	redirect_uri := c.Query("redirect_uri")
+	//	if len(c.Query("scope")) == 0 {
+	//		scope = c.Query("scope")
+	//	}
+	//	db.Update(&AuthRequest{
+	//		ResponseType: code,
+	//		ClientId:     clientId,
+	//		RedirectUri:  redirect_uri,
+	//		Scope:        scope,
+	//	})
+	//	c.Redirect(http.StatusPermanentRedirect, "/server/" + redirect_uri + "?code=" + code)
+	//})
 	//g1 := server.Group("/server/" + redirect_uri)
-	path := ""
-	json := ClientInfo{}
+	//path := ""
+	//json := ClientInfo{}
 		//through json, get clientID, clientPassword
 
 
 	// in server redirect page, get code
 	// it needs to add a verifying operation
-	server.GET("/server/redirect", func(c *gin.Context){
-		getCode := c.Query("code")
-		fmt.Println(getCode)
-		// store code in db1?
+	//server.GET("/server/redirect", func(c *gin.Context){
+	//	getCode := c.Query("code")
+	//	fmt.Println(getCode)
+	//	// store code in db1?
+	//})
+
+
+
+	server.GET("/server/authorization_endpoint", func(c *gin.Context){
+		//responseType := c.Query("response_type")
+		clientId := c.Query("client_id")
+		redirectUri := c.Query("redirect_uri")
+		//scope := c.Query("scope")
+		state := c.Query("state")
+		//codeChallenge := c.Query("code_challenge")
+		//codeChallengeMethod := c.Query("code_challenge_method")
+
+		// temporary
+		AuthorizationCode := clientId
+		path := "http://localhost:9090/server/" + redirectUri + "?code=" + AuthorizationCode + "&state=" + state
+		c.Redirect(302, path)
 	})
 
-
-
-	// APP send JSON format information to service
-	server.POST("/server/redirect", func(c * gin.Context){
-		err = c.BindJSON(&json)
-			fmt.Printf("%+v\n", &json)
-			//path := "/server/oauth/token?client_id=" + json.ClientId +
-			//	"&client_secret=" + json.ClientPassword + "&grant_type=" + json.GrantType +
-			//	"&code=" + json.Code + "&redirect_uri=" + json.RedirectUri
-
-			AuthorizationCode := "ccccrrrr"
-			// through authorization, issue an authorization code to app
-			c.Redirect(http.StatusPermanentRedirect, "http://localhost:9090/app/getresource?code=" + AuthorizationCode)
-			c.JSON(http.StatusOK, gin.H{
-				"authorizationCode": "200",
-			})
-			//c.Redirect(http.StatusPermanentRedirect, path)
+	// server receive code and issue an access token
+	server.POST("/server/token_endpoint", func(c *gin.Context){
+		//grantType := c.PostForm("grant_type")
+		//authorizationCode := c.PostForm("code")
+		//redirectUri := c.PostForm("redirect_uri")
+		//codeVerifier := c.PostForm("code_verifier")
+		c.JSON(http.StatusOK, gin.H{
+			"access_token": "",
+			"token_type": "",
+			"expires_in": "",
+			"refresh_token":"",
+			"scope": "",
 		})
-	app := server.Group("/app")
-
-	// app receive authorization code
-	app.GET("/getresource", func(c *gin.Context){
-
 	})
-	server.POST(path, func(c *gin.Context){
-				c.JSON(http.StatusOK, json)
-		})
 	//4. APP display the authorization page
 	//5. user input ID and password and choose life span
 	//6. service issue an authorization code to App
